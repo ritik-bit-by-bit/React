f88d12e02935275fbf9ebd4153f104d34a62abd5
ritik-bit-by-bit <ritikroshanyadav9696@gmail.com>
2025-02-15 13:13:26 +0530

    assigment complete

--- Assigment-Week-6/src/App.jsx
+++ Assigment-Week-6/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useMemo, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [fact, setFact] = useState(1);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <input
+        type="number"
+        onChange={(e) => setFact(Number(e.target.value))}
+        placeholder="Enter the number here"
+      />
+      <br />
+      Factorial of {fact} is {<Factorial number={fact} />}
+    </div>
+  );
 }
 
-export default App
+ Factorial=useMemo(function Factorial({ number }) {
+  if (isNaN(number) || number < 0) return "Invalid input";
+
+  if (number === 0) return 1;
+
+  let fact = 1;
+  for (let i = number; i >= 1; i--) {
+    fact *= i;
+  }
+
+  return fact;
+},[fact])
+
+export default App;
